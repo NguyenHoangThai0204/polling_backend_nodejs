2f65306ded124ed5bb139a462a4ea99e82f573f3
NguyenHoangThai0204 <thaidp2002@gmail.com>
2024-11-21 10:16:55 +0700

    done delete

--- src/routes/TheNewRoute.js
+++ src/routes/TheNewRoute.js
@@ -10,12 +10,16 @@ const theNewController = require("../controller/TheNewController");
 //     "noiDungBaiViet":"Cuộc sống bận rộn",
 //     "nguoiViet":"Admin"
 // }
-router.post("/createTheNew", theNewController.createTheNew);
+router.post("/create_the_new", theNewController.createTheNew);
 
-// Get: http://localhost:3000/api/theNew/findAllTheNew
-router.get("/findAllTheNew", theNewController.findAllTheNew);
+// Get: http://localhost:3000/api/theNew/find_all_the_new
+router.get("/find_all_the_new", theNewController.findAllTheNew);
 
-// Get: http://localhost:3000/api/theNew/findAllTheNewById
-router.get("/findAllTheNewById/:id", theNewController.findAllTheNewById);
+// Get: http://localhost:3000/api/theNew/find_the_new_by_id/673c6a191a95c26aec6ae0d7
+router.get("/find_the_new_by_id/:id", theNewController.findAllTheNewById);
+
+// Post http://localhost:3000/api/theNew/delete_the_new
+router.post("/delete_the_new/:id",theNewController.deleteTheNew);
+
 
 module.exports = router;
