5cbe51d5d8c34a25b56a61bcadabe1dfd7ac3930
NguyenHoangThai0204 <thaidp2002@gmail.com>
2024-11-28 20:54:06 +0700

    done

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ const io = socketIo(server);
 
 // Lắng nghe kết nối WebSocket từ client
 io.on('connection', (socket) => {
-  console.log('A user connected');
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
